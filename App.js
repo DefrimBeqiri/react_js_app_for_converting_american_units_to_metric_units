7d78110684288ba412621350cb9f39d5ba6198bb
DefrimBeqiri <162150021+DefrimBeqiri@users.noreply.github.com>
2024-03-13 11:06:57 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UnitConverter from './components/UnitConverter';
+
+function App() {
+  return (
+    <div className="App">
+      <UnitConverter />
+    </div>
+  );
+}
+
+export default App;
